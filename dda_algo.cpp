48d789c798735725d37748cb7dd992199fe4a429
paritosh22777 <103773813+paritosh22777@users.noreply.github.com>
2022-11-20 13:17:57 +0530

    Update dda_algo

--- dda_algo.cpp
+++ dda_algo.cpp
@@ -9,39 +9,23 @@ int main()
 	int gdriver = DETECT;
 	int gmode;
 	initgraph(&gdriver, &gmode, (char *)"");
-	int x1, x2, y1, y2;
+	int x1, x2, y1, y2, dx, dy, steps;
 	cout << "Enter coordinates of first point:\n";
 	cin >> x1 >> y1;
 	cout << "Enter coordinates of second point:\n";
 	cin >> x2 >> y2;
-	float dx, dy, m, minverse;
+	float xIncrement, yIncrement, x = x1, y = y1;
 	dx = x2 - x1;
 	dy = y2 - y1;
-	m = dy/dx;
-	minverse = 1/m;
-	if((abs(m) < 1 && x1 > x2) || (abs(m) > 1 && y1 > y2))
+	steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+	xIncrement = dx / (float) steps;
+	yIncrement = dy / (float) steps;
+	putpixel(round(x), round(y), WHITE);
+	for(int i = 0; i < steps; i++)
 	{
-		swap(x1, x2);
-		swap(y1, y2);
-	}
-	putpixel(x1, y1, WHITE);
-	if(abs(m) < 1)
-	{
-		float y = y1;
-		for(int x = x1 + 1; x <= x2 - 1; x++)
-		{
-			y = y1 + m;
-			putpixel(x1, round(y), WHITE);
-		}
-	}
-	else 
-	{
-		float x = x1;
-		for(int y = y1 + 1; y <= y2 - 1; y++)
-		{
-			x = x1 + minverse;
-			putpixel(round(x1), y, WHITE);
-		}
+		x += xIncrement;
+		y += yIncrement;
+		putpixel(round(x), round(y), WHITE);
 	}
 	getch();
 	return 0;
